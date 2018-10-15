430ee891dc57515e9e958507ecfb8989b4bea832
kyzma <77maksimus@mail.ru>
2018-10-15 19:26:22 +0300

    work_varik_no_stranno

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
 
-ddescribe "Static pages" do
+describe "Static pages" do
 
   subject { page }
 
