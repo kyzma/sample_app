81b2ee08927fe06dadf3f1bd18f97390c82e6ab3
kyzma <77maksimus@mail.ru>
2018-10-15 11:37:22 +0300

    new pages and test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-#require 'spec_helper'
 
 describe "Static pages" do
 
@@ -8,6 +7,22 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
