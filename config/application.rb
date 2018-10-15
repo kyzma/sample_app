be49f8ddb8d2fd73c342478e2fa927203dd749dc
kyzma <77maksimus@mail.ru>
2018-10-15 15:38:33 +0300

    bottstrap work

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,9 @@ Bundler.require(*Rails.groups)
 module SampleApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.load_defaults 5.2
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
