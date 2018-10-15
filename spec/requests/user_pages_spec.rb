ac6cb67e865df1bc1b12e1daeece6b773b77dc2a
kyzma <77maksimus@mail.ru>
2018-10-15 19:39:02 +0300

    kostil

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
-
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
 describe "User pages" do
 
   subject { page }
