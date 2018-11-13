2016b6fd61c0b3ad53e9216555b0155c6aee7f50
kyzma <77maksimus@mail.ru>
2018-11-13 11:58:04 +0300

    ne_pashet_zaebalo_vot_kakogo_hera?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,9 @@ module SessionsHelper
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+  def current_user?(user)
+    user == current_user
+  end
   def sign_in(user)
       remember_token = User.new_remember_token
       cookies.permanent[:remember_token] = remember_token
@@ -28,6 +31,13 @@ module SessionsHelper
         cookies.delete(:remember_token)
         self.current_user = nil
       end
+      def redirect_back_or(default)
+          redirect_to(session[:return_to] || default)
+          session.delete(:return_to)
+        end
 
+        def store_location
+          session[:return_to] = request.url if request.get?
+        end
 
 end
