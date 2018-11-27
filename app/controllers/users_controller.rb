4d91ff64c925ee5c6bcd512663dbf7a8881c091a
kyzma <77maksimus@mail.ru>
2018-11-27 09:45:40 +0300

    micropost_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,12 +44,12 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
       end
-      def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    #  def signed_in_user
+  #    unless signed_in?
+  #      store_location
+  #      redirect_to signin_url, notice: "Please sign in."
+  #    end
+  #  end
 
     def correct_user
       @user = User.find(params[:id])
