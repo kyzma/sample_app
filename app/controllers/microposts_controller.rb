071fc943fefe9452e1a7f51424bb2e30d82ebff3
kyzma <77maksimus@mail.ru>
2018-11-28 10:31:45 +0300

    delete_microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -14,11 +14,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+  def get
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
